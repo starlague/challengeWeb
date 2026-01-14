8ebd153702f298cd9c5c64c9709f480d9a1b4e7e
Number6272 <julienwu2003@icloud.com>
2026-01-14 15:50:28 +0100

    Adding the posibility to post picture

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -9,11 +9,28 @@ class HomeController {
     public function index() {
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $image = null;
+
+            if (!empty($_FILES['image']['name'])) {
+
+                $image = time() . '_' . basename($_FILES['image']['name']);
+
+                $uploadDir = __DIR__ . '/../../public/assets/uploads/';
+                $destination = $uploadDir . $image;
+
+                if (!move_uploaded_file($_FILES['image']['tmp_name'], $destination)) {
+                    die("Erreur lors de l'upload de l'image !");
+                }
+            }
+
             Post::create(
-                1, 
+                1,
                 $_POST['title'],
-                $_POST['content']
+                $_POST['content'],
+                $image
             );
+
             header('Location: /');
             exit;
         }
